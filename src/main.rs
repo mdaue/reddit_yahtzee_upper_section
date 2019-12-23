82c19f5f63b0920ad6310761244ca9df1cbfce63
Matt Daue <mattdaue@gmail.com>
2019-12-22 20:54:09 -0800

    Update to capture intent
    Signed-off-by: Matt Daue <mattdaue@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
+use std::collections::HashMap;
 use std::env;
 use std::process;
 
+fn validate_dice(dice: [i32; 5]) -> bool
+{
+    for i in 0..5 {
+        if dice[i] > 6 || dice[i] < 1 {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 fn top_score(dice: [i32; 5]) -> i32
 {
+    let mut dice_map = HashMap::new();
     let mut top_score: i32 = 0;
-    for i in 1..6 {
-        let score = i as i32 * dice[i - 1];
-        if top_score < score {
+    for _i in 1..6 {
+        dice_map.insert(_i, 0);
+    }
+
+    for _i in 1..6 {
+        dice_map.insert(dice[_i - 1], dice_map[&dice[_i - 1]] + 1);
+    }
+
+    for _i in 1..6 {
+        let score = dice_map[&_i] * _i;
+        if top_score < score
+        {
             top_score = score;
         }
     }
 
-    return top_score;
+    return top_score
 }
 
 fn main() {
@@ -26,6 +48,11 @@ fn main() {
 
     for x in 0..5 {
         dice[x] = args[x + 1].parse::<i32>().unwrap();
+    }
+
+    if !validate_dice(dice) {
+        println!("Valid dice faces are 1 - 6");
+        process::exit(0x2);
     }
 
     println!("Top upper section scoring: {:?}", top_score(dice));
